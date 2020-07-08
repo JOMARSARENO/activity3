e3b221c1bcd412539ac2e4537cbd120e70b2d38b
Jomar Sareno <sarenojomar@gmail.com>
2020-07-09 00:12:35 +0800

    first commit

---
+++ src/queue/Class.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queue;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.Node;
+import /*singlylinkedlist*/.SNode;
+
+/**
+ *
+ * @author Oneras
+ */
+public class Class {
+    
+    
+    
+    public static void main(String[] args){
+        
+        
+        Queue c1 = new Queue(2,"jenny");
+        Queue c2 = new Queue(4,"jasson");
+        Queue c3 = new Queue(6,"jade");
+        Queue c4 = new Queue(8,"jane");
+        Queue c5 = new Queue(10,"junjay");
+            
+        SNode<Queue> customer1 = new SNode<>(c1, null);
+        SNode<Queue> customer2 = new SNode<>(c2, null);
+        SNode<Queue> customer3 = new SNode<>(c3, null);
+        SNode<Queue> customer4 = new SNode<>(c4, null);
+        SNode<Queue> customer5 = new SNode<>(c5,null);
+        
+        QueueList <SNode> transaction = QueueList<>();
+        transaction.enqueue(customer1);
+        transaction.enqueue(customer2);
+        transaction.enqueue(customer3);
+        transaction.enqueue(customer4);
+        transaction.enqueue(customer5);
+        
+        transaction.displayqueue();
+                
+        
+    }
+    
+    
+}
